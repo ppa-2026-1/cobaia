98d46121b77363680ce884cc7e3d85d008f74a44
Marcio <marcio.torres@riogrande.ifrs.edu.br>
2026-05-12 18:28:01 +0000

    half-assed auth flow

---
+++ user-api/src/main/java/com/example/demo/model/AuthService.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    public String validateToken(String token) {
+        
+        return "userhandle";
+    }
+    
+}
